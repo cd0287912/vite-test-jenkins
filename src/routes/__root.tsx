392a1c60551f405c17fbc14dd8f94f5759c4ec48
忘不了oh <cd0287912@126.com>
2026-01-21 17:52:11 +0800

    tst

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,16 @@
-import {
-  createRootRoute,
-  Outlet,
-  useRouterState
-} from "@tanstack/react-router"
+import { createRootRoute, Outlet } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
 
 interface MyRouteContext {
-  title?: string;
+  title?: string
   // 你可以添加更多字段
 }
 
 export const Route = createRootRoute<undefined, MyRouteContext>({
-  component: RootComponent
+  component: RootComponent,
 })
 
 function RootComponent() {
-  const { matches } = useRouterState()
-  const activeMatch = matches[matches.length - 1]
-  const { title = "Default Title" } = activeMatch.context
-  document.title = title
   return (
     <>
       <Outlet />
